ff7ecb36ebf3b78d3c745a1b02165cd1d1c38b15
vinujan <vinujan@bitbucket.org>
2017-03-11 14:07:42 +0530

    exeercise 2 answered

--- machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/machine-learning-ex2/ex2/costFunctionReg.m
@@ -8,8 +8,8 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+J = 1/m*sum(-y.* log(sigmoid(X* theta)) - (1 - y) .* log(1-sigmoid(X * theta )) ) + lambda/(2*m)*sum(theta(2:end,1).^2);
+grad = 1/m * ((sigmoid(X * theta) - y)' * X)' + lambda/m*(theta.*[0; ones(size(theta) - 1,1)]);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
